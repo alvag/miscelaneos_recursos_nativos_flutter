75baa7af3ea406b21c46b564077088aff92813f8
Max Alva Garcia <alva85@gmail.com>
2023-06-16 05:47:55 -0500

    Pantallas y rutas

---
+++ android/app/src/main/kotlin/com/maxalva/miscelaneos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.maxalva.miscelaneos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:miscelaneos/config/config.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+    );
+  }
+}
