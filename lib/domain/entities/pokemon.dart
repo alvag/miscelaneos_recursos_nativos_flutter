f5bf8c33473b9214fe8fae08c34f7696c47070f0
Max Alva Garcia <alva85@gmail.com>
2023-06-16 17:37:04 -0500

    DDD - Domain Driven Design

---
+++ lib/domain/entities/pokemon.dart
@@ -0,0 +1,11 @@
+class Pokemon {
+  final String id;
+  final String name;
+  final String spriteFont;
+
+  const Pokemon({
+    required this.id,
+    required this.name,
+    required this.spriteFont,
+  });
+}
