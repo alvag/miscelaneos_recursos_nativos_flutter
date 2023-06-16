5a7ba349c6b63bb9add7692504dbd3b885e3a86c
Max Alva Garcia <alva85@gmail.com>
2023-06-16 14:49:12 -0500

    Brújula - Pantalla inicial

--- lib/presentation/screens/sensors/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/permissions/permission_provider.dart';
+import 'package:miscelaneos/presentation/screens/permissions/ask_location_screen.dart';
 
-class CompassScreen extends StatelessWidget {
+class CompassScreen extends ConsumerWidget {
   const CompassScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final locationGranted = ref.watch(permissionsProvider).locationGranted;
+
+    if (!locationGranted) {
+      return const AskLocationScreen();
+    }
+
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('CompassScreen'),
+        backgroundColor: Colors.black,
+        title: const Text('Brújula', style: TextStyle(color: Colors.white)),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
       body: const Center(
         child: Text('CompassScreen'),
