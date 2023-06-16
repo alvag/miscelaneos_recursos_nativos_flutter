abb4a942c4204a9973e9dc0e746845bf22939833
Max Alva Garcia <alva85@gmail.com>
2023-06-16 15:18:20 -0500

    Mover la aguja hacia el norte

--- lib/presentation/screens/sensors/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:miscelaneos/presentation/providers/permissions/permission_provider.dart';
-import 'package:miscelaneos/presentation/screens/permissions/ask_location_screen.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
+import 'package:miscelaneos/presentation/screens/screens.dart';
 
 class CompassScreen extends ConsumerWidget {
   const CompassScreen({super.key});
@@ -9,6 +11,7 @@ class CompassScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final locationGranted = ref.watch(permissionsProvider).locationGranted;
+    final compassHeading$ = ref.watch(compassStreamProvider);
 
     if (!locationGranted) {
       return const AskLocationScreen();
@@ -21,15 +24,26 @@ class CompassScreen extends ConsumerWidget {
         title: const Text('Brújula', style: TextStyle(color: Colors.white)),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: const Center(
-        child: Compass(),
+      body: Center(
+        child: compassHeading$.when(
+          data: (heading) => Compass(
+            heading: heading ?? 0,
+          ),
+          error: (error, stackTrace) => Text(
+            '$error',
+            style: const TextStyle(color: Colors.white),
+          ),
+          loading: () => const CircularProgressIndicator(),
+        ),
       ),
     );
   }
 }
 
 class Compass extends StatelessWidget {
-  const Compass({super.key});
+  final double heading;
+
+  const Compass({super.key, required this.heading});
 
   @override
   Widget build(BuildContext context) {
@@ -37,13 +51,17 @@ class Compass extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        const Text('155', style: TextStyle(color: Colors.white, fontSize: 30)),
+        Text('${heading.ceil()}',
+            style: const TextStyle(color: Colors.white, fontSize: 30)),
         const SizedBox(height: 20),
         Stack(
           alignment: Alignment.center,
           children: [
             Image.asset('assets/images/compass/quadrant-2.png'),
-            Image.asset('assets/images/compass/needle-1.png'),
+            Transform.rotate(
+              angle: (heading * (pi / 180) * -1),
+              child: Image.asset('assets/images/compass/needle-1.png'),
+            ),
           ],
         )
       ],
