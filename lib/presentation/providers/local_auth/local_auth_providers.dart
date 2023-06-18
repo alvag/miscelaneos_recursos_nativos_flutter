31005ea089478973e92ab06cd4e53a95ef0f1d97
Max Alva Garcia <alva85@gmail.com>
2023-06-18 18:20:05 -0500

    Riverpod - LocalAuth NotifierProvider

--- lib/presentation/providers/local_auth/local_auth_providers.dart
+++ lib/presentation/providers/local_auth/local_auth_providers.dart
@@ -4,3 +4,51 @@ import 'package:miscelaneos/config/config.dart';
 final canCheckBiometricsProvider = FutureProvider<bool>((ref) async {
   return await LocalAuthPlugin.canCheckBiometrics();
 });
+
+enum LocalAuthStatus { authenticated, notAuthenticated, authenticating }
+
+class LocalAuthState {
+  final bool didAuthenticate;
+  final LocalAuthStatus status;
+  final String message;
+
+  LocalAuthState({
+    this.didAuthenticate = false,
+    this.status = LocalAuthStatus.notAuthenticated,
+    this.message = '',
+  });
+
+  copyWith({
+    bool? didAuthenticate,
+    LocalAuthStatus? status,
+    String? message,
+  }) {
+    return LocalAuthState(
+      didAuthenticate: didAuthenticate ?? this.didAuthenticate,
+      status: status ?? this.status,
+      message: message ?? this.message,
+    );
+  }
+}
+
+class LocalAuthNotifier extends StateNotifier<LocalAuthState> {
+  LocalAuthNotifier() : super(LocalAuthState());
+
+  Future<(bool, String)> authenticateUser() async {
+    final (didAutenticate, message) = await LocalAuthPlugin.authenticate();
+    state = state.copyWith(
+      didAuthenticate: didAutenticate,
+      status: didAutenticate
+          ? LocalAuthStatus.authenticated
+          : LocalAuthStatus.notAuthenticated,
+      message: message,
+    );
+
+    return (didAutenticate, message);
+  }
+}
+
+final localAuthProvider =
+    StateNotifierProvider<LocalAuthNotifier, LocalAuthState>((ref) {
+  return LocalAuthNotifier();
+});
