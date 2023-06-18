6f21a56e1b8d76e59e7dad92e2dcae155f542e7e
Max Alva Garcia <alva85@gmail.com>
2023-06-18 18:10:45 -0500

    Riverpod - LocalAuth Providers

--- lib/presentation/screens/biometrics/biometric_screen.dart
+++ lib/presentation/screens/biometrics/biometric_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/providers.dart';
 
-class BiometricScreen extends StatelessWidget {
+class BiometricScreen extends ConsumerWidget {
   const BiometricScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final canCheckBiometrics = ref.watch(canCheckBiometricsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Biometric Screen'),
@@ -16,6 +20,11 @@ class BiometricScreen extends StatelessWidget {
             FilledButton(
               onPressed: () {},
               child: const Text('Autenticar'),
+            ),
+            canCheckBiometrics.when(
+              data: (canCheck) => Text('Can check: $canCheck'),
+              error: (error, _) => Text('Error: $error'),
+              loading: () => const CircularProgressIndicator(),
             ),
             const SizedBox(height: 40),
             const Text('Estado del biometrico', style: TextStyle(fontSize: 30)),
