e8a96cbae1a935776a032319423827cdfd19b57f
Max Alva Garcia <alva85@gmail.com>
2023-06-19 07:15:51 -0500

    Mostrar un mapa básico

--- lib/presentation/screens/location/map_screen.dart
+++ lib/presentation/screens/location/map_screen.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:miscelaneos/presentation/providers/location/user_location_provider.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends ConsumerWidget {
   const MapScreen({super.key});
 
   @override
+  Widget build(BuildContext context, ref) {
+    final currentPositionAsync = ref.watch(userLocationProvider);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('MapScreen Screen'),
+        ),
+        body: currentPositionAsync.when(
+          data: (data) => _MapView(
+            initialLatitude: data.$1,
+            initialLongitude: data.$2,
+          ),
+          error: (error, stack) => Center(
+            child: Text('Error: $error'),
+          ),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        ));
+  }
+}
+
+class _MapView extends StatefulWidget {
+  final double initialLatitude;
+  final double initialLongitude;
+
+  const _MapView({
+    required this.initialLatitude,
+    required this.initialLongitude,
+  });
+
+  @override
+  State<_MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<_MapView> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('MapScreen Screen'),
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(widget.initialLatitude, widget.initialLongitude),
+        zoom: 12.0,
       ),
+      myLocationEnabled: true,
+      onMapCreated: (GoogleMapController controller) {},
     );
   }
 }
