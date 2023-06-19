7d89b54d468ff54e0dbc41f1e48d2d528c029f6e
Max Alva Garcia <alva85@gmail.com>
2023-06-19 11:23:56 -0500

    Reaccionar a las acciones

--- lib/config/plugins/quick_actions_plugin.dart
+++ lib/config/plugins/quick_actions_plugin.dart
@@ -1,27 +1,30 @@
+import 'package:miscelaneos/config/config.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 class QuickActionsPlugin {
   static registerActions() {
     const QuickActions quickActions = QuickActions();
-    
-    quickActions.initialize((String shortcutType) {
-      print(shortcutType);
 
+    quickActions.initialize((String shortcutType) {
       switch (shortcutType) {
-        case 'biometrics':
+        case 'biometric':
+          appRouter.push('/biometrics');
           break;
         case 'compass':
+          appRouter.push('/compass');
           break;
         case 'pokemons':
+          appRouter.push('/pokemons');
           break;
         case 'charmander':
+          appRouter.push('/pokemons/4');
           break;
       }
     });
 
     quickActions.setShortcutItems(<ShortcutItem>[
       const ShortcutItem(
-        type: 'biometrics',
+        type: 'biometric',
         localizedTitle: 'Biometrics',
         // icon: 'ic_fingerprint',
       ),
